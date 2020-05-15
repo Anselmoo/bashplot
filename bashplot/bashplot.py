72b0db610f796d65af37b547100b45be695a35d9
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-05-15 10:19:04 +0200

    Add bashplot folder

---
+++ bashplot/bashplot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import os
+import argparse
+from pathlib import Path
+
+import numpy as np
+import plotille as plt
+
+if sys.version < "3":
+    print("Unsupported Python Version (version < 3)!")
+    sys.exit(1)
+
+
