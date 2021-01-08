4c8db70479dfe4294c097ce82e5f9a26cf3434ad
Anselm Hahn <Anselm.Hahn@gmail.com>
2021-01-08 18:09:12 +0100

    Change from unitest to pytest

--- test/test_bashplot.py
+++ test/test_bashplot.py
@@ -1,104 +1,127 @@
-#!/usr/bin/env python
-
 """Test for basplot."""
-import unittest
-from glob import glob
+
 from pathlib import Path
+from unittest import mock
+
+import sample_generator
 
 from bashplot import bashplot
 
+sample_generator.generate()
 
-class BashplotTestCase(unittest.TestCase):
-    def call_bashplot(self, args):
-        args = bashplot.get_args()
-        return bashplot.bashplot(args)
+print()
 
-    def setUp(self):
-        self.infile = glob("test*.txt")
-        self.args_1 = {
-            "infile": self.infile,
-            "comments": None,
-            "delimiter": None,
-            "skip_header": 0,
-            "skip_footer": 0,
-            "usecols": (0, 1),
-            "size": [30, 20],
-            "x_limits": None,
-            "y_limits": None,
-            "scatter": False,
-            "color": False,
-            "legend": True,
-            "version": False,
-        }
-        self.args_2 = {
-            "infile": self.infile,
-            "comments": None,
-            "delimiter": None,
-            "skip_header": 2,
-            "skip_footer": 2,
-            "usecols": (0, 1),
-            "size": [60, 40],
-            "x_limits": [0.0, 3.0],
-            "y_limits": [0.0, 3.0],
-            "scatter": True,
-            "color": True,
-            "legend": False,
-            "version": True,
-        }
-        # Example with different delimiter
-        self.singlefile = [Path("./test_data_mixed.dat")]
-        self.args_3 = {
-            "infile": self.singlefile,
-            "comments": None,
-            "delimiter": "\t",
-            "skip_header": 2,
-            "skip_footer": 2,
-            "usecols": (0, 1, 2, 3),
-            "size": [40, 30],
-            "x_limits": [0.0, 3.0],
-            "y_limits": [0.0, 3.0],
-            "scatter": False,
-            "color": True,
-            "legend": True,
-            "version": True,
-        }
+test_txt = Path.cwd().glob("test*.txt")
+test_dat = Path.cwd().glob("test*.dat")
 
-    def test_fnames(self):
-        self.assertEqual(
-            bashplot.get_args(opt={"infile": self.infile})["infile"], self.infile
-        )
-
-    def test_options(self):
-        self.assertTrue(bashplot.get_args(opt={"scatter": True})["scatter"])
-        self.assertTrue(bashplot.get_args(opt={"color": True})["color"])
-        self.assertFalse(bashplot.get_args(opt={"legend": False})["legend"])
-        self.assertTrue(bashplot.get_args(opt={"version": True})["version"])
-
-    def test_ranges(self):
-        self.assertEqual(
-            bashplot.get_args(opt={"x_limits": [0.0, 20.0]})["x_limits"], [0.0, 20.0]
-        )
-        self.assertEqual(
-            bashplot.get_args(opt={"y_limits": [0.0, 20.0]})["y_limits"], [0.0, 20.0]
-        )
-        self.assertEqual(bashplot.get_args(opt={"size": [60, 60]})["size"], [60, 60])
-        self.assertEqual(bashplot.get_args()["size"], [60, 40])
-        self.assertEqual(
-            bashplot.get_args(opt={"usecols": [0, 2, 5]})["usecols"], [0, 2, 5]
-        )
-
-    def test_default_run(self):
-        bashplot.bashplot(fnames=self.infile, args=self.args_1)
-        assert 1
-
-    def test_customize_run_1(self):
-        bashplot.bashplot(fnames=self.infile, args=self.args_2)
-        assert 1
-
-    def test_customize_run_2(self):
-        bashplot.bashplot(fnames=self.singlefile, args=self.args_3)
-        assert 1
+args_1 = {
+    "infile": test_txt,
+    "comments": None,
+    "delimiter": None,
+    "skip_header": 0,
+    "skip_footer": 0,
+    "usecols": (0, 1),
+    "size": [30, 20],
+    "x_limits": None,
+    "y_limits": None,
+    "scatter": False,
+    "color": False,
+    "legend": True,
+    "version": False,
+}
+args_2 = {
+    "infile": test_txt,
+    "comments": None,
+    "delimiter": None,
+    "skip_header": 2,
+    "skip_footer": 2,
+    "usecols": (0, 1),
+    "size": [60, 40],
+    "x_limits": [0.0, 3.0],
+    "y_limits": [0.0, 3.0],
+    "scatter": True,
+    "color": True,
+    "legend": False,
+    "version": True,
+}
+# Example with different delimiter
+args_3 = {
+    "infile": test_dat,
+    "comments": None,
+    "delimiter": "\t",
+    "skip_header": 2,
+    "skip_footer": 2,
+    "usecols": (0, 1, 2, 3),
+    "size": [40, 30],
+    "x_limits": [0.0, 3.0],
+    "y_limits": [0.0, 3.0],
+    "scatter": False,
+    "color": True,
+    "legend": True,
+    "version": True,
+}
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_fnames():
+
+    bashplot.get_args(opt={"infile": test_txt})["infile"] == test_txt
+
+
+def test_options_scatter():
+    bashplot.get_args(opt={"scatter": True})["scatter"] == True
+
+
+def test_options_color():
+    bashplot.get_args(opt={"color": True})["color"] == True
+
+
+def test_options_legend():
+    bashplot.get_args(opt={"legend": False})["legend"] == False
+
+
+def test_options_version():
+    bashplot.get_args(opt={"version": True})["version"] == True
+
+
+def test_xlimits():
+    bashplot.get_args(opt={"x_limits": [0.0, 20.0]})["x_limits"] == [0.0, 20.0]
+
+
+def test_ylimits():
+    bashplot.get_args(opt={"y_limits": [0.0, 20.0]})["y_limits"] == [0.0, 20.0]
+
+
+def test_size():
+    bashplot.get_args(opt={"size": [60, 60]})["size"] == [60, 60]
+
+
+def test_ranges_default():
+    bashplot.get_args()["size"] == [60, 40]
+
+
+def test_usecols():
+    bashplot.get_args(opt={"usecols": [0, 2, 5]})["usecols"] == [0, 2, 5]
+
+
+@mock.patch("bashplot.bashplot.bashplot")
+def test_default_run(bashplot):
+    bashplot.bashplot(fnames=test_txt, args=args_1)
+    assert bashplot.bashplot.is_called
+
+
+@mock.patch("bashplot.bashplot.bashplot")
+def test_customize_run_1(bashplot):
+    bashplot.bashplot(fnames=test_txt, args=args_2)
+    assert bashplot.bashplot.is_called
+
+
+@mock.patch("bashplot.bashplot.bashplot")
+def test_customize_run_2(bashplot):
+    bashplot.bashplot(fnames=test_dat, args=args_3)
+    assert bashplot.bashplot.is_called
+
+
+@mock.patch("bashplot.bashplot.command_line_runner")
+def test_command_line(command_line_runner):
+    bashplot.command_line_runner()
+    assert bashplot.command_line_runner.is_called
